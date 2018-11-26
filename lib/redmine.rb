c28012858f22c9575c2e3d2cb0325e630d86d064
Administrator <tamadmin@TamarackBoise.local>
2018-11-26 12:42:42 -0700

    Main menu tweaks - Home and Wiki

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -186,9 +186,12 @@ Redmine::AccessControl.map do |map|
 end
 
 Redmine::MenuManager.map :top_menu do |menu|
-  menu.push :home, :home_path
+  menu.push :home, "https://helpdesk.tamarackidaho.com/projects/it-helpdesk/issues?set_filter=1&tracker_id=1"
+  # menu.push :home, :home_path
+  # menu.push :home, :top_menu, :all_issues, { :controller => 'issues', :action => 'index', :project_id => @project, :set_filter => 1}, :last => true, :caption => :label_issue_all
   menu.push :my_page, { :controller => 'my', :action => 'page' }, :if => Proc.new { User.current.logged? }
   menu.push :projects, { :controller => 'projects', :action => 'index' }, :caption => :label_project_plural
+  menu.push :wiki, "https://helpdesk.tamarackidaho.com/projects/it-helpdesk/wiki", :caption => "Wiki"
   menu.push :administration, { :controller => 'admin', :action => 'index' }, :if => Proc.new { User.current.admin? }, :last => true
   menu.push :help, Redmine::Info.help_url, :last => true
 end
